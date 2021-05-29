0f7cd2dfb82d04fec6f3bf0493051cd7cac63fee
Garrett Armstrong <garrett.w.armstrong@gmail.com>
2021-05-29 18:58:49 -0400

    cyoa json parser

---
+++ 03-cyoa/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Story map[string]Page
+
+type Page struct {
+	Title   string   `json:"title"`
+	Story   []string `json:"story"`
+	Options []struct {
+		Text string `json:"text"`
+		Arc  string `json:"arc"`
+	} `json:"options"`
+}
+
+func parseJSON(filepath string) Story {
+	var s Story
+	b, err := os.ReadFile(filepath)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(b, &s)
+	if err != nil {
+		panic(err)
+	}
+	return s
+}
+
+func main() {
+	tale := parseJSON("gopher.json")
+
+	fmt.Printf("%+v\n", tale)
+}
